2acf34f2cb7c5d4bc58999b27cf0112c2c75c30e
Masood Rezazadeh <rezazadehmasood@gmail.com>
2025-07-28 20:51:08 +0330

    2025-jul-28

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,86 +1,55 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -gx POCKETBASE_URL https://pb.framers.top
+
+# https://how.wtf/disable-welcome-message-in-fish-shell.html
 set -g fish_greeting
 
-function starship_transient_rprompt_func
-  starship module time
-end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-if status is-interactive
-    starship init fish | source
+set -gx MINICONDA3_HOME "$HOME/miniconda3"
+set -gx PATH "$MINICONDA3_HOME/bin" $PATH
 
-    #enable_transience
-end
+set -gx PATH "$HOME/my-scripts" $PATH
 
-#eval (ssh-agent -c) > /dev/null
+export STARSHIP_CONFIG=$HOME/.config/starship.toml
 
-
-set -x EDITOR "nvim"
-# set -x TERM "kitty"
-set -x XDG_CONFIG_HOME "$HOME/.config"
-
-set -x OBSIDIAN_VAULT "$HOME/Documents/My Notes"
-set -x ELECTRON_OZONE_PLATFORM_HINT wayland
-
-
-# List Directory
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
-
-# Handy change dir shortcuts
-abbr .. 'cd ..'
-abbr ... 'cd ../..'
-abbr .3 'cd ../../..'
-abbr .4 'cd ../../../..'
-abbr .5 'cd ../../../../..'
-
-# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
-abbr mkdir 'mkdir -p'
-
+starship init fish | source
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/roy/miniconda3/bin/conda
-    eval /home/roy/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f /Users/masood/miniconda3/bin/conda
+    eval /Users/masood/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "/home/roy/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/roy/miniconda3/etc/fish/conf.d/conda.fish"
+    if test -f "/Users/masood/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/Users/masood/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/home/roy/miniconda3/bin" $PATH
+        set -x PATH "/Users/masood/miniconda3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
 
-set -gx XDG_CURRENT_DESKTOP "KDE"
-set -gx XDE_FULL_SESSION "true"
+source ~/.asdf/asdf.fish
 
-set -gx PATH "/home/roy/.config/composer/vendor/bin:/usr/local/go/bin" $PATH
+set -gx PATH "$HOME/.asdf/installs/deno/$(asdf current deno | awk '{print $2}')/.deno/bin" $PATH
 
-set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
+set -gx PATH "$HOME/.gem/bin" $PATH
 
-set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --follow --glob '!.git'"
+set -gx PATH "$HOME/.bun/bin" $PATH
 
-set -gx PATH "$(go env GOBIN):$(go env GOPATH)/bin" $PATH
+set -gx PATH "$HOME/development/flutter/bin" $PATH
 
-zoxide init fish | source
+set -gx PATH $PATH $HOME/.krew/bin
 
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/roy/.cache/lm-studio/bin
+bind \co "tmux-sessionizer"
+bind \cp "anote"
+bind \cq "qnote"
 
-#fish_ssh_agent
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
-#function starship_transient_prompt_func
-#	tput cuu1
-#	starship module character
-#end
-
-#function prompt_newline --on-event fish_postexec
-#	echo ""
-#	echo ""
-#end
-
-alias clear "command clear; commandline -f clear-screen"
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-source "$HOME/.cargo/env.fish"
+# opencode
+fish_add_path /Users/masood/.opencode/bin
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
 function ls --wraps='eza -1   --icons=auto' --wraps=exa --description 'alias ls=exa'
-  exa $argv
+  eza $argv
         
 end
